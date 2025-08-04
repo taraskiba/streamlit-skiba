3f154cb0a1b3f169bc2d69a05045848948d7b10d
taraskiba <tskiba@vols.utk.edu>
2025-08-04 15:02:48 -0400

    continuation

--- pages/2_🪟_Area_Extraction.py
+++ pages/2_🪟_Area_Extraction.py
@@ -91,7 +91,7 @@ def extract_median_values(data, geedata, start_date, end_date, **kwargs):
 
     # Retrieve data from the image using sampleRegions
     sampled_data = gm.extract_values_to_points(fc, geeimage, scale = None)
-
+    print(dataset_id)
     return sampled_data
 
 @st.cache_data
